a929b359de1ae1d5de888f2022d2376888f189c3
Flamarion França <132231462+GazuaDev8417@users.noreply.github.com>
2023-07-03 19:53:05 -0300

    Update index.jsx

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,4 +1,4 @@
-huimport { useEffect, useState } from 'react'
+jihuimport { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { url } from '../../constants/url'
@@ -13,7 +13,7 @@ import './login.css'
 export default function Login(props){
     const navigate = useNavigate()
     const [form, setForm] = useState({
-        email:promo_prime@email.com',
+        email:'promo_prime@email.com',
         password:'123456'
     })
 
