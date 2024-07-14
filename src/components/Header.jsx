6e5c11ffb29fcd53a00b371625e1be701330f227
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2024-07-14 13:50:39 -0300

    ajustes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ export default function Header({leftItem, rightItem}){
     return(
         <header>
             <span>{leftItem}</span>
-            <img src="https://www.promoprime.com.br/promo/wp-content/themes/promo-prime/img/promo-prime.png" alt="Logo"/>
+            <div>Contratos</div>
+            {/* <img src="https://www.promoprime.com.br/promo/wp-content/themes/promo-prime/img/promo-prime.png" alt="Logo"/> */}
             <span>{rightItem}</span>
         </header>
     )
